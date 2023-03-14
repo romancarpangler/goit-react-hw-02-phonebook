5ccd0078c0feee24fa02eabc56891fa50cc40a4c
romancarpangler <korzunroman56@gmail.com>
2023-03-14 14:05:41 +0200

    gg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,71 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { FormAddcontacts } from 'form';
 import { Contacts } from 'contacts';
 import { Filter } from 'filter';
 import { server } from 'server';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     const saveContact = localStorage.getItem('contacts');
+    console.log(saveContact);
     if (saveContact) {
       const contactsParse = JSON.parse(saveContact);
-      this.setState({ contacts: contactsParse });
+      setContacts(contactsParse);
+
       return;
     }
-    this.setState({ contacts: server });
-  }
+    setContacts(server);
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  onSubmit = ({ name, number }) => {
+  const onSubmit = (name, number) => {
     const contact = {
       id: nanoid().toString(),
       name,
       number,
     };
 
-    if (this.state.contacts.find(contact => contact.name === name)) {
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
       return alert(`${name} is already in contacts.`);
     }
 
-    this.setState({
-      contacts: [...this.state.contacts, contact],
-    });
+    setContacts(prevState => [...prevState, contact]);
   };
 
-  onChenge = e => {
-    this.setState({ filter: e.target.value }, this.filterName);
+  const onChenge = event => {
+    setFilter(event.target.value);
   };
 
-  filterName() {
-    const valueFilter = this.state.filter.toLowerCase();
-    const contacts = this.state.contacts;
-    return contacts.filter(contact => contact.name.includes(valueFilter));
-  }
-
-  deleteContact = id => {
-    const contacts = this.state.contacts;
-    this.setState(() => ({
-      contacts: contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <FormAddcontacts onSubmit={this.onSubmit} />
-
-        <h1>Contacts</h1>
-        <Filter onChenge={this.onChenge} />
-        <Contacts
-          contacts={this.filterName()}
-          onDeleteContact={this.deleteContact}
-        />
-      </div>
+  const filterName = () => {
+    const valueFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(valueFilter)
     );
-  }
-}
+  };
+
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <FormAddcontacts onSubmit={onSubmit} />
+
+      <h1>Contacts</h1>
+      <Filter onChenge={onChenge} />
+      <Contacts contacts={filterName()} onDeleteContact={deleteContact} />
+    </div>
+  );
+};
